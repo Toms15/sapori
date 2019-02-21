7db3d0be357f2abf85ffb5970b85e2e5389a5124
Toms15 <info@tommasocostantini.it>
2019-02-21 11:19:07 +0100

    wp-config

--- wp-config-local.php
+++ wp-config-local.php
@@ -23,10 +23,10 @@
 define('DB_NAME', 'sapori-catering');
 
 /** MySQL database username */
-define('DB_USER', 'admin');
+define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'admin');
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
