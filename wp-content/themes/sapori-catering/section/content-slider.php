989330c5f1202c73689e198842c240011be1aec0
Toms15 <info@tommasocostantini.it>
2019-03-06 17:19:00 +0100

    Slide Homepage

--- wp-content/themes/sapori-catering/section/content-slider.php
+++ wp-content/themes/sapori-catering/section/content-slider.php
@@ -14,12 +14,10 @@ $images = get_sub_field('immagini');
 <section class="section__content section__content--slider">
 	<div class="swiper-container" id="swiper--home">
 		<div class="swiper-wrapper">
-			<div class="swiper-slide">
-				01
-			</div>
-			<div class="swiper-slide">
-				02
-			</div>
+			<?php foreach($images as $image): ?>
+				<div class="swiper-slide" style="background: url(<?php echo $image['url']; ?>) no-repeat center center;">
+				</div>
+			<?php endforeach; ?>
 		</div>
 		<div class="swiper-button-prev"></div>
 		<div class="swiper-button-next"></div>
